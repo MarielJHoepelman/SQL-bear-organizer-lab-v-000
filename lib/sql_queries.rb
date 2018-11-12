c64e7951dbecf4ec70a30255bb09621a12ba6f19
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-11-12 19:11:39 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT temperament, COUNT(temperament) FROM bears ORDER BY COUNT(temperament) WHERE temperament = 'goofy';"
+  "COUNT(temperament) FROM bears WHERE temperament = 'goofy' ORDER BY COUNT(temperament);"
 end
 
 def selects_bear_that_killed_Tim
